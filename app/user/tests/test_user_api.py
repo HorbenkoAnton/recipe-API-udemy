de27f9adbba199a08e6fdb23fb23f1ee62dacc60
HorbenkoAnton <gorbenko.anton.punk2077@gmail.com>
2025-05-14 11:36:52 +0200

    fixed typo

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -57,7 +57,7 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code, status.HTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = (
             get_user_model()
             .objects.filter(
